0205e847b23096fe993078ff2bbabb33ae82890b
PC <PC@DESKTOP-UOP3VF7>
2022-04-07 22:15:50 -0500

    Puse un Hola

---
+++ src/gestionalmacen/GestionAlmacen.java
@@ -0,0 +1,17 @@
+
+package gestionalmacen;
+
+public class GestionAlmacen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola soy jonataN");
+        System.out.println("Hola soy jonataN2");
+        //Hola
+         
+       
+    }
+    
+}
