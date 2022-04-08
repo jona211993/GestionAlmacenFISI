63dd25a15a747b577cbed852cb5636c386a1221d
alex <alex.lizarazo@unmsm.edu.pe>
2022-04-07 22:09:26 -0500

    Borre mi nombre

--- src/gestionalmacen/GestionAlmacen.java
+++ src/gestionalmacen/GestionAlmacen.java
@@ -8,7 +8,7 @@ public class GestionAlmacen {
      */
     public static void main(String[] args) {
         System.out.println("Hola soy jonataN");
-        System.out.println("Hola soy Alex");
+        
        
     }
     
